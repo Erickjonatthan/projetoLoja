70dede014a3a4b21f424e704a26ee07ff1ec883f
Erick Jonathan <65685466+Erickjonatthan@users.noreply.github.com>
2024-10-11 14:14:28 -0300

    melhorando o design

--- js/script.js
+++ js/script.js
@@ -1,13 +1,55 @@
 let count = 1;
 document.getElementById("radio1").checked = true;
-setInterval(function(){
- nedtImage();
-},5000)
 
-function nedtImage(){
+let intervalId = setInterval(nextImage, 5000);
+let timeoutId;
+
+function nextImage() {
     count++;
-    if(count>4){
-        count=1;
+    if (count > 3) {
+        count = 1;
     }
-    document.getElementById("radio"+count).checked = true;
+    document.getElementById("radio" + count).checked = true;
 }
+
+function prevImage() {
+    count--;
+    if (count < 1) {
+        count = 3;
+    }
+    document.getElementById("radio" + count).checked = true;
+}
+
+function disableAutoSlide() {
+    clearInterval(intervalId);
+}
+
+function restartAutoSlide() {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(function() {
+    intervalId = setInterval(nextImage, 5000);
+    }, 500);
+}
+
+function nextSlide() {
+    disableAutoSlide();
+    nextImage();
+    restartAutoSlide();
+}
+
+function prevSlide() {
+    disableAutoSlide();
+    prevImage();
+    restartAutoSlide();
+}
+
+function manualBtnClicked() {
+    disableAutoSlide();
+    restartAutoSlide();
+}
+
+// Adiciona eventos de clique aos elementos com a classe 'manual-btn'
+document.querySelectorAll('.manual-btn').forEach(btn => {
+    btn.addEventListener('click', manualBtnClicked);
+});
+
